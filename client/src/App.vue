5ae3ab13d4f0dab95ce7517aa0a416192fdc1810
kdjordan <k.dean.jordan@gmail.com>
2023-02-03 07:00:26 -0800

    added page transitions

--- client/src/App.vue
+++ client/src/App.vue
@@ -9,7 +9,22 @@
 			<Navbar />
 		</header>
 		<main className="h-screen">
-			<router-view></router-view>
+			<router-view v-slot="{ Component }">
+				<transition name="fade" mode="out-in">
+					<component :is="Component" />
+				</transition>
+			</router-view>
 		</main>
 	</div>
 </template>
+
+<style>
+	.fade-enter-active,
+	.fade-leave-active {
+		transition: opacity .5s;
+	}
+	.fade-enter,
+	.fade-leave-to {
+		opacity: 0;
+	}
+</style>
